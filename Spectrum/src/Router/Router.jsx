dc70d6dbfa0e879b7bcb34742cbde244219531c1
MARIA EDUARDA KNUPP RATTO <maria_ratto@estudante.sesisenai.org.br>
2025-04-14 16:32:24 -0300

    Tela de Pessoa interessada

--- Spectrum/src/Router/Router.jsx
+++ Spectrum/src/Router/Router.jsx
@@ -2,8 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import Tela_Inicial from "../Front-end/Tela_Inicial";
 import FuncionarioCadastro from "../Front-end/Funcionarios/Cadastro/FuncionariosCadastro.jsx";
 import PerfilFuncionario from "../Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx";
+import Cadastro_interesse from "../Tenho-Interesse/Cadastro_interesse.jsx";
 const Router = createBrowserRouter([
-{path: "/", element: <Tela_Inicial/> },
+{path: "/", element: <Cadastro_interesse/> },
 {path: "funcionarioC", element: <FuncionarioCadastro/> },
 {path: "perfilF", element: <PerfilFuncionario/>}
     
