911a1cc0b9e9e6f537d21965b2fe6a54cab0b319
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-04-14 17:20:27 -0300

    Manuel

--- Spectrum/src/Front-end/Funcionarios/NavBarFuncionario/NavbarFuncionario.jsx
+++ Spectrum/src/Front-end/Funcionarios/NavBarFuncionario/NavbarFuncionario.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './NavBarFuncionarios.css'
 function NavbarFuncionario() {
   return (
     <div className='container-advogados-nav'>   
     <div className='container-advogados-nav-dois'>
-        <img  src="Logo Advocacia.png" alt="" />
+        <img  src="Spectrum.jpeg" alt="" />
   <div className='container-advogados-nav-tres'>
       <div class="button-group">
          <input type="radio" id="svelt" name="frameworks" />
@@ -12,7 +12,7 @@ function NavbarFuncionario() {
   </div>
       <div class="button-group">
          <input type="radio" id="react" name="frameworks" />
-         <label for="react">Processos</label>
+         <label for="react">Agenda</label>
   </div>
 
   <div class="button-group">
--- Spectrum/src/Router/Router.jsx
+++ Spectrum/src/Router/Router.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Tela_Inicial from "../Front-end/Tela_Inicial";
 import FuncionarioCadastro from "../Front-end/Funcionarios/Cadastro/FuncionariosCadastro.jsx";
 import PerfilFuncionario from "../Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx";
+import TelaIFuncionario from "../Front-end/Funcionarios/TelaInicial_Funcionario/TelaIFuncionario.jsx";
 const Router = createBrowserRouter([
 {path: "/", element: <Tela_Inicial/> },
 {path: "funcionarioC", element: <FuncionarioCadastro/> },
-{path: "perfilF", element: <PerfilFuncionario/>}
+{path: "perfilF", element: <PerfilFuncionario/>},
+{path: "TelaIFuncionario", element: <TelaIFuncionario/>}
     
 ])
 
