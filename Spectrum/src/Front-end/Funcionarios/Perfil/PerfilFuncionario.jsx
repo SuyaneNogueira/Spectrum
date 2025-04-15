fb4adfa1e4bbcb27f51b2f1cf894cf516b8cd332
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-04-15 17:10:18 -0300

    Manuel

--- Spectrum/src/Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx
+++ Spectrum/src/Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx
@@ -3,9 +3,25 @@ import './PerfilFuncionario.css'
 
 function PerfilFuncionario() {
   return (
-    <div>PerfilFuncionario
+    <div className='container-geral-funcionarios'>
+      <div className='nao-navbar-funcionario'>
+        <div className='div-imagem-funcionarios'>
+          <img src="tela de contrato.png" alt="" />
+         </div>
+         <div className='titulo-funcionarios-perfil'>
+         <h1>Perfil do Funcionario</h1>
+   </div>
+     <div className='botao-perfil-funcionarios'>
+       <button className='botao-voltar-perfil-funcionarios'>VOLTAR</button>
+    </div>
+</div>
+    <div className='all-divs-funcionarios'>
+       <div className='div-um-funcionarios'>  
+          </div>
+     <div className='div-dois-funcionarios'> 
+   </div>       
+</div>
 
-      
     </div>
   )
 }
--- Spectrum/src/Router/Router.jsx
+++ Spectrum/src/Router/Router.jsx
@@ -3,12 +3,14 @@ import Tela_Inicial from "../Front-end/Tela_Inicial";
 import FuncionarioCadastro from "../Front-end/Funcionarios/Cadastro/FuncionariosCadastro.jsx";
 import PerfilFuncionario from "../Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx";
 import Cadastro_interesse from "../Tenho-Interesse/Cadastro_interesse.jsx";
-import Slider from "../Front-end/Slider/Slider.jsx";
+import Slider from "../Front-end/Slider/Slider.jsx";    
 
 const Router = createBrowserRouter([
+    
 {path: "/", element: <Tela_Inicial/> },
 {path: "funcionarioC", element: <FuncionarioCadastro/> },
 {path: "perfilF", element: <PerfilFuncionario/>},
+
 ])
 
 export default Router
