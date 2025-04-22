966813adda80067be71feb5e91c958ba069b03d1
MANUEL ALEJANDRO MUÑOZ GONZÁLEZ <manuel_gonzalez@estudante.sesisenai.org.br>
2025-04-22 17:23:23 -0300

    Manuel

--- Spectrum/src/Router/Router.jsx
+++ Spectrum/src/Router/Router.jsx
@@ -7,20 +7,11 @@ import Slider from "../Front-end/Slider/Slider.jsx";
 import LoginFuncionario from "../Front-end/Funcionarios/Login/LoginFuncionario.jsx";
 
 const Router = createBrowserRouter([
-<<<<<<< HEAD
-{path: "/", element: <FuncionarioCadastro/> },
-=======
 {path: "/", element: <Tela_Inicial/> },
->>>>>>> 91fcae5acf0401dcf889b2962e13a4af12190a6f
-{path: "funcionarioC", element: <FuncionarioCadastro/> },
-<<<<<<< HEAD
-// {path: "perfilF", element: <PerfilFuncionario/>}
-    
-=======
+{path: "funcionarioC", element: <FuncionarioCadastro/>},
 {path: "perfilF", element: <PerfilFuncionario/>},
 {path: "login", element:<LoginFuncionario/>}
 
->>>>>>> 8364c305f868401c70798d955e0b0715a3d7a4ec
 ])
 
 export default Router
