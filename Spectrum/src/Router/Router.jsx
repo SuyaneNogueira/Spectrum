7b639bdbce3647e8c42c6ead8d969d696338f695
MARIA EDUARDA KNUPP RATTO <maria_ratto@estudante.sesisenai.org.br>
2025-04-24 14:40:16 -0300

    Atualização da tela inicial

--- Spectrum/src/Router/Router.jsx
+++ Spectrum/src/Router/Router.jsx
@@ -1,19 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Tela_Inicial from "../Front-end/Tela_Inicial";
 import FuncionarioCadastro from "../Front-end/Funcionarios/Cadastro/FuncionariosCadastro.jsx";
-import PerfilFuncionario from "../Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx";
-import Cadastro_interesse from "../Tenho-Interesse/Cadastro_interesse.jsx";
-import Slider from "../Front-end/Slider/Slider.jsx";    
+import PerfilFuncionario from "../Front-end/Funcionarios/Perfil/PerfilFuncionario.jsx";   
 import LoginFuncionario from "../Front-end/Funcionarios/Login/LoginFuncionario.jsx";
 
 const Router = createBrowserRouter([
-<<<<<<< HEAD
-{path: "/", element: <Tela_Inicial/> },
+{path: "/inicio", element: <Tela_Inicial/> },
 {path: "funcionarioC", element: <FuncionarioCadastro/>},
-=======
-{path: "/", element: <Cadastro_interesse/> },
-{path: "funcionarioC", element: <FuncionarioCadastro/> },
->>>>>>> 7d7cb428cfaf1955bc37034422fc681c9746f1a1
 {path: "perfilF", element: <PerfilFuncionario/>},
 {path: "login", element:<LoginFuncionario/>}
 
