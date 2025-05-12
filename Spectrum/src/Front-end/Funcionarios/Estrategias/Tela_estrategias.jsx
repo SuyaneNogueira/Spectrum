c78efacdf63755a3992354a26816fc1f315c9fb0
MARIA EDUARDA KNUPP RATTO <maria_ratto@estudante.sesisenai.org.br>
2025-05-12 17:18:53 -0300

    Modal e atualizações da tela de estrategias

--- Spectrum/src/Front-end/Funcionarios/Estrategias/Tela_estrategias.jsx
+++ Spectrum/src/Front-end/Funcionarios/Estrategias/Tela_estrategias.jsx
@@ -1,9 +1,19 @@
 import Navbar_estrategias from "./Navbar_estrategias"
 import "./Tela_estrategias.css"
+import Modal_estrategias from './Modal_estrategias'; // Ajuste o caminho conforme necessário
+import { useState } from "react";
+
 
 
 
 function Tela_estrategias() {
+  
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div className='container_principal_tela_estrategias'>
       <div className="container_segundaria_estrategias">
@@ -21,16 +31,21 @@ function Tela_estrategias() {
         <div className="elements_texts_topicos_estrategias">
           <div className="elements_texts_estrategias">
             <h1 className="sons_altos_estrategias">Sons Altos</h1>
-            <h2 className="text_sons_altos_estrategias">Lidar com sons autos pode ser particularmente desafiador para pessoas autistas, mas algumas 
+            <h2 className="text_sons_altos_estrategias">Lidar com sons altos pode ser particularmente desafiador para pessoas autistas, mas algumas 
             estratégias podem ajudar a minimizar o desconforto:</h2>
           </div>
           <div className="container_buttons_style_estrategias">
-            <button className="buttons_style_estrategias">Protetores Auriculares</button>
-            <button className="buttons_style_estrategias">Espaços Tranquilos</button>
-            <button className="buttons_style_estrategias">Planejamento</button>
-            <button className="buttons_style_estrategias">Comunicação</button>
-            <button className="buttons_style_estrategias">Técnicas de relaxamento</button>
-            <button className="buttons_style_estrategias">Música ou Ruído Branco</button>
+          {/* <select className='input1-interesse2'>
+            <option value="nao">Não</option>
+            <option value="sim">Sim</option>
+          </select> */} 
+            <button className="buttons_style_estrategias"   onClick={toggleModal} >  <Modal_estrategias isOpen={isModalOpen} onClose={toggleModal}> </Modal_estrategias> Protetores Auriculares</button>
+            <button className="buttons_style_estrategias"  >Espaços Tranquilos</button>
+            <button className="buttons_style_estrategias"  >Planejamento</button>
+            <button className="buttons_style_estrategias"  >Comunicação</button>
+            <button className="buttons_style_estrategias"  >Técnicas de relaxamento</button>
+            <button className="buttons_style_estrategias"  >Música ou Ruído Branco</button>
+            
           </div>
         </div>
         </div>
