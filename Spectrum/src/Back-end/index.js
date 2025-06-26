9fdedb56cb7d33d37210fa1925348d871e27fc97
SUYANE CONCEIÇÃO NOGUEIRA <suyane_nogueira@estudante.sesisenai.org.br>
2025-06-26 16:42:49 -0300

    atualizações

--- Spectrum/src/Back-end/index.js
+++ Spectrum/src/Back-end/index.js
@@ -1,10 +1,20 @@
+require("dotenv").config(); 
+
+const db = require("./db");
+
+const port = process.env.PORT;
+
 const express = require('express');
-const cors = require('cors');
-const { Pool } = require('pg');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+
+// app.use(express.json());
+
+
+
+app.listen(port);
+
+console.log("Backend Rodando!")
 
 
 
